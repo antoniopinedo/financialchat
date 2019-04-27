a14f15f2140fc1da44df1c6f621a8c1b8a14a66c
Antonio Pinedo <antoniop@mill5.com>
2019-04-26 23:48:25 -0500

    Added basic structure

---
+++ FinancialChat/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinancialChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            bool authenticated = (System.Web.HttpContext.Current.User != null) && System.Web.HttpContext.Current.User.Identity.IsAuthenticated;
+
+            if (!authenticated)
+            {
+                return Redirect("/Account/Login");
+            }
+
+            return View();
+        }
+    }
+}
